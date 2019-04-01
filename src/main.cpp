521ba3d6c14297f69f46e4b97fe3b16bba8cd6ce
Jean-Raymond Raynal <jeanraymond.raynal@gmail.com>
2019-03-31 21:01:51 -0400

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,90 @@
-#include <cstdint>
 #include <SFML/Graphics.hpp>
+#include <cstdint>
 #include <entt/entt.hpp>
 
-struct position {
-  float x;
-  float y;
+struct position
+{
+	float x;
+	float y;
 };
 
-struct velocity {
-  float dx;
-  float dy;
+struct velocity
+{
+	float dx;
+	float dy;
 };
 
-void draw(sf::RenderWindow& window, entt::registry<> &registry) {
-  auto transform = registry.view<position, sf::RectangleShape>();
+void
+draw(sf::RenderWindow& window, entt::registry<>& registry)
+{
+	auto transform = registry.view<position, sf::RectangleShape>();
 
-  for (auto entity : transform) {
-    auto pos   = transform.get<position>(entity);
-    auto shape = transform.get<sf::RectangleShape>(entity);
-	shape.setPosition(pos.x, pos.y);
-    window.draw(shape);
-  }
+	for (auto entity : transform)
+	{
+		auto pos = transform.get<position>(entity);
+		auto shape = transform.get<sf::RectangleShape>(entity);
+		shape.setPosition(pos.x, pos.y);
+		window.draw(shape);
+	}
 }
 
-void update(float dt, entt::registry<> &registry) {
-  registry.view<position, velocity>().each([dt](const auto, auto &pos, const auto &vel) {
-    // gets all the components of the view at once ...
+void
+update(float dt, entt::registry<>& registry)
+{
+	registry.view<position, velocity>().each([dt](const auto, auto& pos, const auto& vel) {
+		// gets all the components of the view at once ...
 
-    pos.x += vel.dx * dt;
-    pos.y += vel.dy * dt;
+		pos.x += vel.dx * dt;
+		pos.y += vel.dy * dt;
 
-    // ...
-  });
+		// ...
+	});
 }
 
-int main() {
-  entt::registry registry;
+int
+main()
+{
+	entt::registry registry;
 
-  for (auto i = 0; i < 10; ++i) {
-    auto entity = registry.create();
-    registry.assign<position>(entity, i * 1.f, i * 1.f);
-    registry.assign<sf::RectangleShape>(entity, sf::Vector2f{10.0f, 10.0f});
+	for (auto i = 0; i < 10; ++i)
+	{
+		auto entity = registry.create();
+		registry.assign<position>(entity, i * 1.f, i * 1.f);
+		registry.assign<sf::RectangleShape>(entity, sf::Vector2f{10.0f, 10.0f});
 
-    if (i % 2 == 0) {
-      registry.assign<velocity>(entity, i * 10.0f, i * 10.0f);
-    }
-  }
+		if (i % 2 == 0)
+		{
+			registry.assign<velocity>(entity, i * 10.0f, i * 10.0f);
+		}
+	}
 
-  sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Conan adventure begins!", sf::Style::Close);
-  sf::Clock clock;
+	sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Conan adventure begins!", sf::Style::Close);
+	sf::Clock clock;
 
-  while (sfmlWin.isOpen()) {
+	while (sfmlWin.isOpen())
+	{
 
-    sf::Event e;
-    while (sfmlWin.pollEvent(e)) {
+		sf::Event e;
+		while (sfmlWin.pollEvent(e))
+		{
 
-      switch (e.type) {
-      case sf::Event::EventType::Closed:
-        sfmlWin.close();
-        break;
-      }
-    }
+			switch (e.type)
+			{
+				case sf::Event::EventType::Closed:
+					sfmlWin.close();
+					break;
+			}
+			case sf::Event::EvenType::KeyPressed:
+			{
+			}
+		}
 
-    update(clock.getElapsedTime().asSeconds(), registry);
+		update(clock.getElapsedTime().asSeconds(), registry);
 
-    sfmlWin.clear(sf::Color(174, 198, 207));
-    draw(sfmlWin, registry);
-    sfmlWin.display();
-	clock.restart();
-  }
-  return 0;
+		sfmlWin.clear(sf::Color(174, 198, 207));
+		draw(sfmlWin, registry);
+		sfmlWin.display();
+		clock.restart();
+	}
+	return 0;
 }
